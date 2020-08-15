274bef38845b062c22d345c1929f6ef0f46b41cf
Dain Sundstrom <dain@iq80.com>
2020-08-15 14:06:30 -0700

    Rename ScalarFunctionImplementation to SpecializedSqlScalarFunction

--- core/trino-main/src/main/java/io/trino/operator/scalar/MapToMapCast.java
+++ core/trino-main/src/main/java/io/trino/operator/scalar/MapToMapCast.java
@@ -106,7 +106,7 @@ public final class MapToMapCast
     }
 
     @Override
-    public ScalarFunctionImplementation specialize(BoundSignature boundSignature, FunctionDependencies functionDependencies)
+    public SpecializedSqlScalarFunction specialize(BoundSignature boundSignature, FunctionDependencies functionDependencies)
     {
         checkArgument(boundSignature.getArity() == 1, "Expected arity to be 1");
         MapType fromMapType = (MapType) boundSignature.getArgumentType(0);
@@ -121,7 +121,7 @@ public final class MapToMapCast
         BlockPositionIsDistinctFrom keyEqual = blockTypeOperators.getDistinctFromOperator(toKeyType);
         BlockPositionHashCode keyHashCode = blockTypeOperators.getHashCodeOperator(toKeyType);
         MethodHandle target = MethodHandles.insertArguments(METHOD_HANDLE, 0, keyProcessor, valueProcessor, toMapType, keyEqual, keyHashCode);
-        return new ChoicesScalarFunctionImplementation(boundSignature, NULLABLE_RETURN, ImmutableList.of(NEVER_NULL), target);
+        return new ChoicesSpecializedSqlScalarFunction(boundSignature, NULLABLE_RETURN, ImmutableList.of(NEVER_NULL), target);
     }
 
     /**
